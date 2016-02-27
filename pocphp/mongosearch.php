c10ce0d6fe71f0a0702058d4b7afe51ce65cc229
Prashant Atri <Pratri@Prashants-MacBook-Pro.local>
2016-02-27 13:33:32 -0500

    One field serach on error field, working fine, and printing in Table format. Also resolved undefined index issue.

--- pocphp/mongosearch.php
+++ pocphp/mongosearch.php
@@ -9,17 +9,6 @@
         var_dump($doc);
     }
 
-
-/* Working code */
-  echo "<h2> searching </h2>";
-
-  $sweetquery = array('error' => $_GET["msearch"]);
-
-  $match_results = $collection->find($sweetquery);
-  foreach ($match_results as $doc1){
-      var_dump($doc1);
-  }
-
   echo "<h2> Table printing </h2>";
   $data  = "<table style='border:1px solid red;";
   $data .= "border-collapse:collapse' border='1px'>";
@@ -47,27 +36,38 @@ foreach($cursor as $document){
   echo $data;
 
   /* Working code */
-    echo "<h2> searching and printing in Table format </h2>";
-    $data  = "<table style='border:1px solid red;";
-    $data .= "border-collapse:collapse' border='1px'>";
-    $data .= "<thead>";
-    $data .= "<tr>";
-    $data .= "<th>Database Name</th>";
-    $data .= "<th>Error</th>";
-    $data .= "<th>Solution</th>";
-    $data .= "<th>Vendor Case ID</th>";
-    $data .= "</tr>";
-    $data .= "</thead>";
-    $data .= "<tbody>";
-    $sweetquery = array('error' => $_GET["msearch"]);
+echo "<h2> searching and printing in Table format </h2>";
+  $data  = "<table style='border:1px solid red;";
+  $data .= "border-collapse:collapse' border='1px'>";
+  $data .= "<thead>";
+  $data .= "<tr>";
+  $data .= "<th>Database Name</th>";
+  $data .= "<th>Error</th>";
+  $data .= "<th>Solution</th>";
+  $data .= "<th>Vendor Case ID</th>";
+  $data .= "</tr>";
+  $data .= "</thead>";
+  $data .= "<tbody>";
+  $sweetquery = array('error' => $_GET["msearch"]);
+
 
 $match_results = $collection->find($sweetquery);
 foreach($match_results as $document){
   $data .= "<tr>";
   $data .= "<td>" . $document["dbname"]."</td>";
   $data .= "<td>" . $document["error"]."</td>";
+  if(isset($document["Solution"])){
   $data .= "<td>" . $document["Solution"]."</td>";
+   }
+   else{
+  $data .= "<td>" . "Sorry No Solution Available - If you got one, please update this record"."</td>";
+   }
+  if(isset($document["Vendorcase"])){
   $data .= "<td>" . $document["Vendorcase"]."</td>";
+   }
+  else{
+    $data .= "<td>" ."No Vendor Case Raised yet - If Case raised please update this record"."</td>";
+  }
   $data .= "</tr>";
 }
   $data .= "</tbody>";
