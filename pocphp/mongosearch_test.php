458fe5a77a671afb57bc4db93752020daf4de317
Prashant Atri <Pratri@Prashants-MacBook-Pro.local>
2016-03-01 22:27:44 -0500

    table formating changed

--- pocphp/mongosearch_test.php
+++ pocphp/mongosearch_test.php
@@ -16,19 +16,6 @@ Search Error: <input type="text" name="msearch"><br>
 
 $curtoarry = iterator_to_array($cursor);
 
-echo "<h2> Searching and printing in Table format </h2>";
-  $data  = "<table style='border:1px solid red;";
-  $data .= "border-collapse:collapse' border='1px'>";
-  $data .= "<thead>";
-  $data .= "<tr>";
-  $data .= "<th>Database Name</th>";
-  $data .= "<th>Error</th>";
-  $data .= "<th>Solution</th>";
-  $data .= "<th>Vendor Case ID</th>";
-  $data .= "</tr>";
-  $data .= "</thead>";
-  $data .= "<tbody>";
-
 $searchtext = $_GET["msearch"];
 
 global $results;
@@ -42,19 +29,11 @@ foreach ($curtoarry as $document){
       }
 }
 
-foreach ($results as $document1) {
-  # code...
-  echo "<br>";
-  echo "NExt for loop";
-  print_r($document1);
-
-}
-
 /* Ends here Cursor to array conversion and search inside the array */
 
-echo "<h2> Table printing </h2>";
-$data  = "<table style='border:1px solid red;";
-$data .= "border-collapse:collapse' border='1px'>";
+echo "<h2> Searching and printing in Table format </h2>";
+$data  = "<table style='border:2px solid red;";
+$data .= "border-collapse:collapse' border='2px'>";
 $data .= "<thead>";
 $data .= "<tr>";
 $data .= "<th>Database Name</th>";
@@ -88,5 +67,42 @@ $data .= "</tbody>";
 $data .= "</table>";
 echo $data;
 
+/*print all documents on screen*/
+
+echo "<h2> Table printing </h2>";
+$data  = "<table style='border:1px solid red;";
+$data .= "border-collapse:collapse' border='1px'>";
+$data .= "<thead>";
+$data .= "<tr>";
+$data .= "<th>Database Name</th>";
+$data .= "<th>Error</th>";
+$data .= "<th>Solution</th>";
+$data .= "<th>Vendor Case ID</th>";
+$data .= "</tr>";
+$data .= "</thead>";
+$data .= "<tbody>";
+
+
+foreach($cursor as $document){
+$data .= "<tr>";
+$data .= "<td>" . $document["dbname"]."</td>";
+$data .= "<td>" . $document["error"]."</td>";
+if(isset($document["Solution"])){
+$data .= "<td>" . $document["Solution"]."</td>";
+ }
+ else{
+$data .= "<td>" . "Sorry No Solution Available - If you got one, please update this record"."</td>";
+ }
+if(isset($document["Vendorcase"])){
+$data .= "<td>" . $document["Vendorcase"]."</td>";
+ }
+else{
+  $data .= "<td>" ."No Vendor Case Raised yet - If Case raised please update this record"."</td>";
+}
+$data .= "</tr>";
+}
+$data .= "</tbody>";
+$data .= "</table>";
+echo $data;
 
 ?>
