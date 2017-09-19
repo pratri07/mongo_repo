76d3fb98205927b6f112e9be5846d6eb7baae090
Prashant Atri <prashant_atri2003@yahoo.com>
2017-09-18 21:40:02 -0400

    Update mongoinsert.php

--- pocphp/mongoinsert.php
+++ pocphp/mongoinsert.php
@@ -1,7 +1,7 @@
 <?php
 
-    $m = new MongoClient("mongodb://mongodb", array("username" => user8MT, "password" => TM8NoRUhuIkuaC5T));
-    $db=$m->selectDB("mydb");
+    $m = new MongoClient("mongodb://admin:GMTqnHwxYDEsFHtO@mongodb", array("db" => "admin"));
+    $db=$m->selectDB("admin");
     $collection = new MongoCollection($db,'errorinfo');
 
     $person = array( "dbname" => $_POST["dname"],"error" => $_POST["edesc"],"Solution" => $_POST["solu"],"Vendorcase" => $_POST["vendor"] );
